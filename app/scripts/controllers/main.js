0990a9fe7f88f6531d4a619856b47cb1c04a8c7d
Sebz <smoran@sierrawireless.com>
2014-01-05 19:25:39 +0100

    Added text displaying the latest performed action

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,9 +4,9 @@ angular.module('roquettesApp')
   .controller('MainCtrl', function ($scope, $http) {
 
     var postUrl = '/postData';
-    var buildEvent = function() {
+    var buildEvent = function(time) {
         var result = {
-            timestamp : moment().valueOf(),
+            timestamp : time.valueOf(),
             value : 1
         };
         if ($scope.forceDate) {
@@ -15,37 +15,48 @@ angular.module('roquettesApp')
 
         return result;
     };
+
+    var displayResult = function(action, time) {
+        $scope.action = action;
+        $scope.time = time.format('Do MMMM YYYY, h:mm');
+    };
     $scope.loaded = function($event) {
         jQuery($event.target).button('loading');
+        var time = moment();
         var data = [{
-            'stove.loaded' : [ buildEvent() ]
+            'stove.loaded' : [ buildEvent(time) ]
         }];
         $http.post(postUrl, data).then(function(result) {
             jQuery($event.target).button('reset');
+            displayResult('Loaded', time);
             console.log('# Loaded ! >>', result);
         });
     };
       
     $scope.started = function($event) {
         jQuery($event.target).button('loading');
+        var time = moment();
         var data = [{
-            'stove.state' : [ buildEvent() ]
+            'stove.state' : [ buildEvent(time) ]
         }];
         $http.post(postUrl, data).then(function(result) {
             jQuery($event.target).button('reset');
+            displayResult('Started', time);
             console.log('# Started ! >>', result);
         });
     };
       
     $scope.stopped = function($event) {
         jQuery($event.target).button('loading');
-        var stoppedEvent = buildEvent();
+        var time = moment();
+        var stoppedEvent = buildEvent(time);
         stoppedEvent.value = 0;
         var data = [{
             'stove.state' : [ stoppedEvent ]
         }];
         $http.post(postUrl, data).then(function(result) {
             jQuery($event.target).button('reset');
+            displayResult('Stopped', time);
             console.log('# Stopped ! >>', result);
         });
     };
